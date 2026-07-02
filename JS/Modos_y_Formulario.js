d09baf186d02ec5f435e16cfe64b0504a075603b
Alejo <alejogaston.rios@esrudiantes.unahur.edu.ar>
2026-07-02 12:36:08 -0300

    Modificacion JS/Modos y formularios (mensajes), CSS.Contacto, CSS.TP y Contacto.html

--- JS/Modos_y_Formulario.js
+++ JS/Modos_y_Formulario.js
@@ -4,3 +4,33 @@ btnModo.addEventListener("click",()=>{document.body.classList.toggle("modo-oscur
     btnModo.textContent = modoActivo?"Modo claro" : "Modo oscuro";
 
 })
+
+// Capturamos el formulario, el input del email y el div del mensaje
+const formContacto = document.getElementById("form");
+const inputNombre = document.getElementById("nombre");
+const inputEmail = document.getElementById("email");
+const msgForm = document.getElementById("msg-form");
+const inputTel = document.getElementById("telefono");
+
+formContacto.addEventListener("submit", (e) => {e.preventDefault();
+
+const nombre = inputNombre.value.trim();
+const email = inputEmail.value.trim();
+const telefono = inputTel.value.trim();
+
+if (email.length < 4 || !email.includes("@")) {
+    msgForm.textContent = "Por favor, ingresá un email válido.";
+    msgForm.style.color = "#DE1919";
+    return; 
+    }
+
+if (telefono.length < 8) {
+        msgForm.textContent = "Por favor, ingresá un número de teléfono válido.";
+        msgForm.style.color = "#DE1919";
+        return;
+}
+    msgForm.textContent = `¡Gracias ${nombre}! Hemos recibido tus datos. Te contactaremos a la brevedad.`;
+    msgForm.style.color = "#1A6B2A"
+
+    formContacto.reset();
+});
