b244f8516b3ff854b7f3fe05a470f9456def6c7c
Alejo <alejogaston.rios@esrudiantes.unahur.edu.ar>
2026-07-02 22:00:53 -0300

    ultimas modificaciones leves (estetica)

--- JS/Modos_y_Formulario.js
+++ JS/Modos_y_Formulario.js
@@ -1,11 +1,10 @@
-const modoActivo = document.body.classList.contains("modo-oscuro");
 const btnModo = document.getElementById("btn-modo");
-btnModo.addEventListener("click",()=>{document.body.classList.toggle("modo-oscuro");
-    btnModo.textContent = modoActivo?"Modo claro" : "Modo oscuro";
+btnModo.addEventListener("click", () => {
+    document.body.classList.toggle("modo-oscuro");
+    const modoActivo = document.body.classList.contains("modo-oscuro");    
+    btnModo.textContent = modoActivo ? "Modo claro 🌞" : "Modo oscuro 🌑";
+});
 
-})
-
-// Capturamos el formulario, el input del email y el div del mensaje
 const formContacto = document.getElementById("form");
 const inputNombre = document.getElementById("nombre");
 const inputEmail = document.getElementById("email");
